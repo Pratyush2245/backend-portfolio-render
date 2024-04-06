603b29f89897b586b23ab2d5b7768aa9ce264d6a
PratyushBhattacharya123 <pratyushbhattacharya33@gmail.com>
2024-04-06 09:27:14 +0530

    updated-domain

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ exports.app.use((0, cookie_parser_1.default)());
 exports.app.use(
   (0, cors_1.default)({
     // origin: ["http://localhost:3000"],
-    origin: ["https://portfolio-frontend-ten-omega.vercel.app"],
+    origin: ["https://profile-frontend-rust.vercel.app"],
     credentials: true,
   })
 );
