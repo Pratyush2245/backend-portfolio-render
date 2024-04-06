96da49965537632e3398c7757e9b8238e89f3a1c
PratyushBhattacharya123 <pratyushbhattacharya33@gmail.com>
2024-04-06 07:13:49 +0530

    updated-domain

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
@@ -20,31 +22,40 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 // cookie parser
 exports.app.use((0, cookie_parser_1.default)());
 // enable CORS(cross origin resource sharing)
-exports.app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000"],
+exports.app.use(
+  (0, cors_1.default)({
+    // origin: ["http://localhost:3000"],
+    origin: ["https://my-portfolio-frontend-green.vercel.app"],
     credentials: true,
-}));
+  })
+);
 // api request limit
 const limiter = (0, express_rate_limit_1.rateLimit)({
-    windowMs: 15 * 60 * 1000,
-    limit: 100,
-    standardHeaders: "draft-7",
-    legacyHeaders: false,
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
 });
 // routes
-exports.app.use("/api/v1", user_route_1.default, project_route_1.default, layout_route_1.default, contact_route_1.default);
+exports.app.use(
+  "/api/v1",
+  user_route_1.default,
+  project_route_1.default,
+  layout_route_1.default,
+  contact_route_1.default
+);
 // testing api
 exports.app.get("/test", (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        message: "API is working",
-    });
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
 });
 // unknown routes
 exports.app.all("*", (req, res, next) => {
-    const err = new Error(`Route ${req.originalUrl} not found`);
-    err.statusCode = 404;
-    next(err);
+  const err = new Error(`Route ${req.originalUrl} not found`);
+  err.statusCode = 404;
+  next(err);
 });
 // Middleware calls
 // Apply the rate limiting middleware to all requests.
